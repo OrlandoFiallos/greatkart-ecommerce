1e2fdccd2b5f70be9280f8e5e5aeeca9c2bd221d
OrlandoFiallos <contactofiallosorlando@gmail.com>
2023-04-24 16:15:05 -0600

    Starting product variations & variation manager

--- carts/models.py
+++ carts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from store.models import Product
+from store.models import Product, Variation
 
 class Cart(models.Model):
     cart_id = models.CharField(max_length=255, blank=True)
@@ -10,11 +10,12 @@ class Cart(models.Model):
 
 class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variations = models.ManyToManyField(Variation, blank=True)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
     
-    def __str__(self) -> str:
+    def __unicode__(self) -> str:
         return self.product
     
     def subtotal(self):
--- store/models.py
+++ store/models.py
@@ -19,3 +19,27 @@ class Product(models.Model):
     
     def get_absolute_url(self):
         return reverse('products_detail', args=[self.category.slug, self.slug])
+
+VARIATION_CATEGORY_CHOICES = (
+    ('color','color'),
+    ('size','size'),
+)
+
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager, self).filter(variation_category='color', is_active=True)
+    
+    def sizes(self):
+        return super(VariationManager, self).filter(variation_category='size', is_active=True)
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=5, choices=VARIATION_CATEGORY_CHOICES)
+    variation_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now=True)
+    
+    objects = VariationManager()
+    
+    def __str__(self) -> str:
+        return self.variation_value
