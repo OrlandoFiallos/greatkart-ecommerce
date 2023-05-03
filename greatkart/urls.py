7b2627d1ea1bd6966e8335ad9c79710b0e342bd6
OrlandoFiallos <contactofiallosorlando@gmail.com>
2023-05-03 13:27:46 -0600

    Orders & Order number generation

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('store/', include('store.urls')),
     path('store/cart/', include('carts.urls')),
     path('users/', include('users.urls')),
+    path('orders/', include('orders.urls')),
     
     
 ] + static(settings.MEDIA_URL, document_root=  settings.MEDIA_ROOT)
